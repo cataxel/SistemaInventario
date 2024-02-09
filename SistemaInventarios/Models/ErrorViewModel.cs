ef37185903aee8357ed52b8146dcb3a15c0ef36a
Axel Lopez <axellolopez2002@gmail.com>
2024-02-09 09:27:48 -0600

    primer commit

---
+++ SistemaInventarios/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaInventarios.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
